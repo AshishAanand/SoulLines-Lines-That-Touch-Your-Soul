3cd36ca6456747085b58f13ed124977e378de266
AshishAanand <programmerashish7@gmail.com>
2025-10-21 18:08:33 +0530

    Created Server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,44 @@
 import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import cors from 'cors';
 
-console.log("Starting server...");
+dotenv.config();
+
+// Initialize Express app
+const app = express();
+
+
+
+// ############################## ( Middlewares ) ##############################
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors(
+    { origin: '*' }
+));
+
+
+
+// ############################## ( Routes ) ###################################
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// ############################## ( Error Handling ) ###########################
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+
+
+// ############################ (Server setup) ##############################
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
