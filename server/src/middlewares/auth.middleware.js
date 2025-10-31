8e713cbcf278be4e0144f0a901f60778accf53bb
AshishAanand <programmerashish7@gmail.com>
2025-10-31 21:13:47 +0530

    fixed simple issue

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import User from '../models/user.model.js';
+import User from '../models/User.model.js';
 import TokenBlacklist from '../models/tokenBlackList.model.js';
 
 const protect = async (req, res, next) => {
